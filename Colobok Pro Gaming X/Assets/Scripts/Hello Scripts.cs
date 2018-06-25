19e25c24b904b7c7749874869d67f09307b9c581
GrumpyMonk <murom.d@mail.ru>
2018-06-25 18:34:14 +0300

    Create New Project Colobok Pro Gaming X

---
+++ Colobok Pro Gaming X/Assets/Scripts/Hello Scripts.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Привет Григорий");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
